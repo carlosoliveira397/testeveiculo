5ccf6be77c7c9fa721da0c5a22d5da51ac327ca9
carlosoliveira397 <carlosd.oliveira397@gmail.com>
2018-10-25 04:53:10 +0900

    change adicionado no banco.js e pesquisaum.php (no c9)

--- www/components/banco.js
+++ www/components/banco.js
@@ -55,3 +55,27 @@ function pesquisar(){
     }
   });
 }
+
+$(document).on("change","#lista", function(){
+  var codigoSelecionado = $("option:selected", ("#lista")).val();
+  $.ajax({
+    type:"get",
+    url:"https://testeveiculoc9-carlos397.c9users.io/pesquisa.php",
+    data:"codigo="+codigoSelecionado,
+    dataType: "json",
+    sucess: function(data){
+       navigator.notification.alert("erro: "+data);
+      $.each(data.veiculo, function(i, data){
+        $("#codigo").val(data.codigo);
+        $("#txtModelo").val(data.modelo);
+        $("#txtCor").val(data.cor);
+        $("#txtFabricante").val(data.fabricante);
+        $("#dtAno").val(data.ano);
+        $("#nrValor").val(data.valor);
+      });
+    },
+    error: function(data){
+      navigator.notification.alert("erro: "+data);
+    }
+  });
+});
